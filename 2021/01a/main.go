639706dd6aa45f1a52c63fe110a10ca8772edca4
Dakota Chambers <dakotachambers@gmail.com>
2021-12-02 12:53:40 -0600

    go fmt

--- 2021/01a/main.go
+++ 2021/01a/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 
-    data,err := os.Open("data.txt")  
-    if err != nil {
-        panic(err)
-    }
+	data, err := os.Open("data.txt")
+	if err != nil {
+		panic(err)
+	}
 
-    defer data.Close()
+	defer data.Close()
 
-    // Read the input file into a Slice
-    var dataArr []int
-    scanner := bufio.NewScanner(data)
+	// Read the input file into a Slice
+	var dataArr []int
+	scanner := bufio.NewScanner(data)
 
-    for scanner.Scan() {
-        value, err2 := strconv.Atoi(scanner.Text())
-        if err2 != nil {
-            panic(err2)
-        }
-        dataArr = append(dataArr, value)
-    }
+	for scanner.Scan() {
+		value, err2 := strconv.Atoi(scanner.Text())
+		if err2 != nil {
+			panic(err2)
+		}
+		dataArr = append(dataArr, value)
+	}
 
-    // Count the number of times each subsquent element in the slice increases
-    count := 0
-    for j:=1; j<len(dataArr); j++ {
-        if dataArr[j] > dataArr[j-1] {
-            count ++
-        }
-    }
+	// Count the number of times each subsquent element in the slice increases
+	count := 0
+	for j := 1; j < len(dataArr); j++ {
+		if dataArr[j] > dataArr[j-1] {
+			count++
+		}
+	}
 
-    fmt.Println(count)
+	fmt.Println(count)
 }
